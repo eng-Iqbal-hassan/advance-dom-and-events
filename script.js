30677e954a907bfeb46fc10990cf0c6572add38f
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2024-12-23 02:16:37 +0500

    minor update

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
+  // As we move a bit up and still the button is seen we click on the button and modal is opened and page is moved to the top. This is the default behavior as the button is actually anchor whose href is # which moves the page by default to top.In order to avoid this thing we use the event.preventDefault();      
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,11 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+// this for loop is the old school we can use the forEach method instead of it. The syntax will be like:
+btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal)) 
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
